f4e0f7177cee34bd1a48a86ea8912ad270e7a1f4
root <biliyy224432@gmail.com>
2023-08-22 00:40:04 +0000

    Modify latency boxplot to 95 percentile latency

--- box_plot.py
+++ box_plot.py
@@ -3,46 +3,51 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    per_lat = (100 - 95) * 5
     xlocs = np.array([1, 2, 3, 4, 5, 6], dtype=np.int32)
     # Load Data 1
-    x1 = np.load('1v1_small_10hz.npy')
+    x1 = np.load('1v1_small_100hz.npy')
     data1 = []
     for i in range(x1.shape[0]):
-        data1.append(x1[i,:])
-    y1_mean = np.mean(x1, axis=1)
+        data1.append(np.sort(x1[i,:])[:-per_lat])
+    y1_mean = np.mean(np.array(data1), axis=1)
     x1 = xlocs
 
     # Load Data 2
-    x2 = np.load('1v5_small_10hz.npy')
+    x2 = np.load('1v10_small_100hz.npy')
     data2 = []
     for i in range(x2.shape[0]):
-        data2.append(x2[i,:])
-    y2_mean = np.mean(x2, axis=1)
+        data2.append(np.sort(x2[i,:])[:-per_lat])
+    y2_mean = np.mean(np.array(data2), axis=1)
     x2 = xlocs
 
     # Plot
     fig, ax = plt.subplots()
-    per = (2.5, 97.5)
-    per = 3.5
-    box_wid = 0.1
+    per = (0.5, 99.5)
+    box_wid = 0.2
     # Plot Data 1
     c1='green'
     plt.plot(x1, y1_mean, c1)
     locs, labels = plt.xticks()
     box_pos1 = x1 - np.array([box_wid/2*1.2], dtype=np.float64)
     bp1 = ax.boxplot(data1, positions=box_pos1 ,showfliers=True, widths=box_wid, whis=per, patch_artist=True,
-            boxprops=dict(facecolor=(0,1,0,0.1), color=c1),
+            boxprops=dict(facecolor=[0, 1, 0, 0.2], color=c1),
             capprops=dict(color=c1),
             whiskerprops=dict(color=c1),
             flierprops=dict(color=c1, markeredgecolor=c1),
             medianprops=dict(color='black'))
+    # q1 = np.percentile(data1, 25, axis=1)
+    # q2 = np.percentile(data1, 50, axis=1)
+    # q3 = np.percentile(data1, 75, axis=1)
+    # perc = per * (q3 - q1)
+    # print((data1[0][data1[0] > (q3 + perc)[0]]).shape[0], (data1[0][data1[0] < (q1 - perc)[0]]).shape[0])
     
     # Plot Data2
     c2='blue'
     plt.plot(x2, y2_mean, c2)
     box_pos2 = x2 + np.array([box_wid/2*1.2], dtype=np.float64)
     bp2 = ax.boxplot(data2, positions=box_pos2 ,showfliers=True, widths=box_wid, whis=per, patch_artist=True,
-            boxprops=dict(facecolor=(0,0,1,0.1), color=c2),
+            boxprops=dict(facecolor=[0, 0, 1, 0.2], color=c2),
             capprops=dict(color=c2),
             whiskerprops=dict(color=c2),
             flierprops=dict(color=c2, markeredgecolor=c2),
@@ -54,7 +59,7 @@ def main():
     plt.xlabel('Payload(B)')
     plt.ylabel('Latency(us )')
     # plt.xlim((-100,2100))
-    plt.ylim((0,2000))
+    # plt.ylim((0,5000))
     plt.title('Latency Comparison - # of Subscribers (30Hz)')
     plt.show()   
     
--- distribution.py
+++ distribution.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 def main():
     xlocs = np.array([1, 2, 3, 4, 5, 6], dtype=np.int32)
     # Load Data 1
-    x1 = np.load('1v10_small_100hz.npy')
+    x1 = np.load('1v1_small_100hz.npy')
     data1 = []
     for i in range(x1.shape[0]):
         data1.append(x1[i,:])
@@ -13,12 +13,12 @@ def main():
     x1 = xlocs
     # print(np.array(data1)[0])
 
-    plt.hist(np.array(data1)[5], bins=100)
+    plt.hist(np.array(data1)[0], bins=100)
     plt.xlabel('Latency(us)')
     plt.ylabel('# of data')
     plt.minorticks_on()
     plt.grid(which="both")
-    # plt.xlim((-100,2100))
+    plt.xlim((-100,2100))
     # plt.ylim((0,2000))
     plt.title('Latency Comparison - # of Subscribers (30Hz)')
     plt.show()   
