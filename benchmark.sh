50a8738e5d3c18ff05e165e8578eb240ac174ced
root <biliyy224432@gmail.com>
2023-08-23 01:54:58 +0000

    Updated benchmark scrip large

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 data=505
-test_type="small"
+test_type="large"
 
 echo "Launching application, please wait!"
 
@@ -28,18 +28,22 @@ if [[ "$test_type" == "small" ]]; then
     done
 else
     # large payloads
-    for m in 1 2 5 10
+    for m in 1 #2 5 10
     do
-        for hz in 1 10 30 50 100
+        for hz in 10 #30 50 100
         do
-            for n in 1 2 5 10
+            for n in 1 #2 5 10
             do
                 ((wait_time=data/hz+2))
                 for payload in 1MB 5MB 10MB 20MB 30MB 50MB
                 do
-                    timeout $wait_time ros2 launch cpp_benchmark broadcast.launch.py payload_size:=1MB frequency:=$hz n_sub:=${n} m_pub:=${m}
+                    timeout $wait_time ros2 launch cpp_benchmark broadcast.launch.py payload_size:=1MB frequency:=$hz n_sub:=${n} m_pub:=${m} &
+                    timeout $wait_time ./sys_stats $payload $hz $n $m & wait
                     python3 data_process.py $hz $n $m $test_type
+                    sleep 0.5
                 done
+                python3 cpu_mem_process.py $hz $n $m $test_type
+                sleep 0.5
             done
         done
     done
